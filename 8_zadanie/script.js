6b9572c5966b8e02079fdf55d829da1cdd950573
Redrikl <124083799+Redrikl@users.noreply.github.com>
2023-12-24 20:49:37 +0300

    2

---
+++ 8_zadanie/script.js
@@ -0,0 +1,187 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const quantityInput = document.getElementById("quantity");
+    const totalCostSpan = document.getElementById("totalCost");
+  
+    function calculateTotal() {
+      let totalCost = 0;
+  
+      const selectedServiceType = document.querySelector(
+        'input[name="serviceType"]:checked'
+      );
+      if (selectedServiceType) {
+        totalCost += parseFloat(selectedServiceType.getAttribute("data-price"));
+      }
+  
+      const optionsSelect = document.getElementById("optionsSelect");
+      if (optionsSelect && optionsSelect.style.display !== "none") {
+        const selectedOption = optionsSelect.options[optionsSelect.selectedIndex];
+        totalCost += parseFloat(selectedOption.getAttribute("data-price"));
+      }
+  
+      const propertyCheckbox = document.getElementById("propertyCheckbox");
+      if (
+        propertyCheckbox &&
+        propertyCheckbox.checked &&
+        propertyCheckbox.style.display !== "none"
+      ) {
+        totalCost += parseFloat(propertyCheckbox.getAttribute("data-price"));
+      }
+  
+      let quantity = parseFloat(quantityInput.value);
+  
+      if (isNaN(quantity) || quantity < 0) {
+        quantity = 0;
+      }
+  
+      totalCost *= quantity;
+  
+      if (!isNaN(totalCost)) {
+        totalCostSpan.textContent = totalCost;
+      } else {
+        totalCostSpan.textContent = "0";
+      }
+    }
+  
+    function updateOptionsAndProperties() {
+      const selectedServiceType = document.querySelector(
+        'input[name="serviceType"]:checked'
+      ).value;
+      const optionsDiv = document.getElementById("optionsDiv");
+      const propertiesDiv = document.getElementById("propertiesDiv");
+  
+      if (selectedServiceType === "type1") {
+        optionsDiv.style.display = "none";
+        propertiesDiv.style.display = "none";
+      } else if (selectedServiceType === "type2") {
+        optionsDiv.style.display = "block";
+        propertiesDiv.style.display = "none";
+      } else {
+        optionsDiv.style.display = "none";
+        propertiesDiv.style.display = "block";
+      }
+    }
+  
+    function handleInput() {
+      updateOptionsAndProperties();
+      calculateTotal();
+    }
+  
+    document
+      .querySelectorAll('input[name="serviceType"]')
+      .forEach(function (radio) {
+        radio.addEventListener("change", handleInput);
+      });
+  
+    document
+      .getElementById("optionsSelect")
+      .addEventListener("change", calculateTotal);
+    document
+      .getElementById("propertyCheckbox")
+      .addEventListener("change", calculateTotal);
+    quantityInput.addEventListener("input", calculateTotal);
+  
+    handleInput();
+  });
+  
+// создание слайдера
+function createSlick(number){
+    $('.slider').not('.slick-initialized').slick({
+        arrows:true,
+        dots:true,
+        slidesToShow:number,
+        autoplay:false,
+        speed:1000,
+        autoplaySpeed:800,
+    });
+}
+
+// вычисление количества картинок для показа
+function calculateNumberOfSlidesToShow(){
+    var sliderWidth = $('.slider').width();
+    var slidesToShow = 0;
+    switch (true) {
+        case (sliderWidth < 500):
+            slidesToShow = 1;
+            break;
+        case (sliderWidth < 767):
+            slidesToShow = 2;
+            break;
+        case (sliderWidth < 1199):
+            slidesToShow = 3;
+            break;
+        case (sliderWidth > 1200):
+            slidesToShow = 3;
+            break;
+    }
+
+    return slidesToShow;
+}
+
+// перезагружаем слайдер по изменене размера окна
+function reloadSlick () {
+    $('.slider').slick('unslick');
+    slidesToShow = calculateNumberOfSlidesToShow();
+    createSlick(slidesToShow);
+}
+
+// вызываем каждый раз по ресайзу
+jQuery(window).on("resize", reloadSlick);
+
+
+
+jQuery(document).ready(function () {
+
+    // инициализация слайдера по загрузке страницы
+    if ($('.slider').length) {
+        setTimeout(function () {
+            slidesToShow = calculateNumberOfSlidesToShow();
+            createSlick(slidesToShow);
+        }, 300);
+    }
+
+
+});
+
+$(document).ready(function() 
+   {  var form = $('#feedbackForm');
+      var responseMessage = $('#responseMessage');
+      var formData = localStorage.getItem('feedbackFormData'); 
+      if (formData) { formData = JSON.parse(formData);
+      form.find('input[type="text"], input[type="email"], input[type="tel"], textarea').each(function() 
+      { var field = $(this);
+         var fieldName = field.attr('name');
+         field.val(formData[fieldName]);
+      });
+             } form.on('change', 'input[type="text"], input[type="email"], input[type="tel"], textarea', function() 
+               { var formData = {};
+                  form.find('input[type="text"], input[type="email"], input[type="tel"], textarea').each(function() { var field = $(this);
+                  var fieldName = field.attr('name'); formData[fieldName] = field.val();
+                  });
+                  localStorage.setItem('feedbackFormData', JSON.stringify(formData));
+               });
+                 
+                 $('#openPopup').click(function(e) 
+                  { e.preventDefault();
+                     $('.popup-overlay').fadeIn(300);
+                     history.pushState(null, null, '#popup');
+                  });
+                        $(window).on('popstate', function() 
+                        { $('.popup-overlay').fadeOut(300);
+                           form.trigger('reset');
+                           localStorage.removeItem('feedbackFormData');
+                           responseMessage.empty();
+                           history.pushState(null, null, window.location.href.split('#')[0]);
+                        });
+                            form.submit(function(e) 
+                            { e.preventDefault();
+                            responseMessage.empty();
+                            var formData = new FormData(this);
+         $.ajax({ url: 'https://formcarry.com/s/Enf6jxAuC-', type: 'POST', data: formData, processData: false, contentType: false, success: function(response) 
+         { responseMessage.text('Форма успешно отправлена!').addClass('success');
+         form.trigger('reset');
+         localStorage.removeItem('feedbackFormData');
+         }, error: function(xhr, status, error) 
+         { responseMessage.text('Форма успешно отправлена!').addClass('success');
+      } });
+   });
+});
